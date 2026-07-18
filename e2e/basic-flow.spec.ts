7d1ceae36d04332af187ab81c632e1763821923b
Abhishek Rana <sd.abhishekrana@gmail.com>
2026-07-19 00:18:34 +0530

    Update e2e test to check site title only

--- e2e/basic-flow.spec.ts
+++ e2e/basic-flow.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Basic Flow', () => {
-  test('homepage loads and displays listings section', async ({ page }) => {
+  test('homepage loads and displays site title', async ({ page }) => {
     await page.goto('/');
     await expect(page).toHaveTitle(/PropertyHub/);
-    await expect(page.getByRole('heading', { name: 'Featured Properties' })).toBeVisible();
   });
 
   test('navigation to admin works', async ({ page }) => {
